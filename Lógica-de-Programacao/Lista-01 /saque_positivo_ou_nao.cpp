94e3d638f972a636dad5e16dc7832e33f7f8e804
Thiago Amaral <thiagoamaraldearaujoelias@gmail.com>
2025-04-10 20:07:25 -0300

    Create saque_positivo_ou_nao.cpp

---
+++ Lógica-de-Programacao/Lista-01 /saque_positivo_ou_nao.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+
+/* Mini mundo: Um caixa eletrônico precisa saber se o valor digitado para saque é positivo.
+ 
+ 
+
+
+	double valorDaConta;
+	
+	cout<<("Insira o valor do saque: ");
+	cin>>(valorDaConta);
+	
+	if(valorDaConta >= 0) 
+	{
+		cout<<("Valor positivo");
+	} 
+	
+	else
+	{
+		cout<<("Valor negativo ou zero");	
+	}
+
+}
