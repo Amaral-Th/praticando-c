d721ba4d4c9ad55c60ab65c3b2c4f1cff9bf4918
Thiago Amaral <thiagoamaraldearaujoelias@gmail.com>
2025-04-10 20:04:42 -0300

    Create area_terreno.cpp

---
+++ Lógica-de-Programacao/Lista-01 /area_terreno.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main( )
+{
+	//4) Faça um programa que leia a largura e comprimento de um terreno e imprima a área do terreno Área = comprimento x largura.
+	
+	float larguraTerreno, comprimentoTerreno, areaTerreno;
+	
+	cout<<("Insira a largura do terreno: ");
+	cin>>(larguraTerreno);
+	
+	cout<<("Insira o comprimento do terreno: ");
+	cin>>(comprimentoTerreno);
+	
+	areaTerreno = larguraTerreno * comprimentoTerreno;
+
+
+	cout<<("Área do terreno : ");
+	cout<<(areaTerreno);
+	
+	
+	
+	
+	
+	
+	
+}
